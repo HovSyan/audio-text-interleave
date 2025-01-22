da262e3a0909baf26a8697872728b5830b12d9eb
Hovhannes Sahakyan <hovhannes@superannotate.com>
2025-01-22 07:53:26 +0100

    Add handling for invalid url

--- app/player.tsx
+++ app/player.tsx
@@ -1,6 +1,6 @@
 import { Text, View } from "react-native";
-import { fetch } from 'expo/fetch';
-import { Audio, AVPlaybackSource } from 'expo-av';
+import { fetch } from "expo/fetch";
+import { Audio, AVPlaybackSource } from "expo-av";
 import { useEffect, useState } from "react";
 import Player from "@/components/Player";
 import { useRoute } from "@react-navigation/native";
@@ -8,12 +8,12 @@ import { useRoute } from "@react-navigation/native";
 export type RouteParams = {
   audioUri: string;
   captionsUri: string;
-}
+};
 
 const PLAYER = new Audio.Sound();
 
 function loadAudio(uri: string) {
-  return new Promise((res) => {
+  return new Promise((res, rej) => {
     /**
      * It seems Audio.Sound has some issues regarding completely loading the media file.
      * There were cases that the video was not loaded, but the promise was resolved.
@@ -21,39 +21,56 @@ function loadAudio(uri: string) {
      */
     let checkCount = 0;
     const checkIfLoaded = async () => {
-      const status = await PLAYER.getStatusAsync()
+      const status = await PLAYER.getStatusAsync();
       if (status.isLoaded && status.durationMillis) {
         return res(status);
       }
       if (checkCount++ > 10) {
-        throw 'Invalid Video/Audio'
+        rej("Invalid Video/Audio");
       }
-      setTimeout(checkIfLoaded, 500);
-    }
+      setTimeout(checkIfLoaded, 1000);
+    };
 
-    PLAYER.loadAsync({ uri }).then(checkIfLoaded);
-  })
+    PLAYER.loadAsync({ uri }).then(checkIfLoaded).catch(rej);
+  });
 }
 
 function loadCaptionsJSON(uri: string) {
-  return fetch(uri).then((r) => r.json())
+  return fetch(uri).then((r) => r.json());
 }
 
 export default function PlayerPage() {
   const router = useRoute();
   const [ready, setReady] = useState(false);
+  const [error, setError] = useState(false);
   const [json, setJson] = useState<any>(null);
 
   useEffect(() => {
     const { audioUri, captionsUri } = router.params as RouteParams;
-    Promise.all([loadAudio(audioUri), loadCaptionsJSON(captionsUri)]).then(([status, json]) => {
-      setJson(json);
-      setReady(true);
+    Promise.all([loadAudio(audioUri), loadCaptionsJSON(captionsUri)]).then(
+      ([status, json]) => {
+        setJson(json);
+        setReady(true);
+      }
+    ).catch((e) => {
+      setError(true)
     });
     return () => {
       PLAYER.unloadAsync();
-    }
-  }, [])
+    };
+  }, []);
+
+  const content = error ? (
+    <Text>
+      It's an error, couldn't load resource from the urls (check CORS for
+      example)
+    </Text>
+  ) : !ready ? (
+    <Text>Loading...</Text>
+  ) : (
+    <Player player={PLAYER} json={json}></Player>
+  );
+
   return (
     <View
       style={{
@@ -62,7 +79,7 @@ export default function PlayerPage() {
         alignItems: "center",
       }}
     >
-      {ready ? <Player player={PLAYER} json={json}></Player> : <Text>Loading...</Text>}
+      {content}
     </View>
   );
 }
